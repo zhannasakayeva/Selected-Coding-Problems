94d4b9f011a9499fc0356773069fee9ea57483c4
Zhanna Sakayeva <97479348+zhannasakayeva@users.noreply.github.com>
2022-05-15 14:52:14 -0500

    peak element

---
+++ GeeksforGeeks/Peak Element.py
@@ -0,0 +1,16 @@
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        if (n == 1) :
+            return 0
+        if (arr[0] >= arr[1]) :
+            return 0
+        if (arr[n - 1] >= arr[n - 2]) :
+            return n - 1
+  
+    # check for every other element
+        for i in range(1, n - 1) :
+  
+        # check if the neighbors are smaller
+            if (arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]) :
+                return i
